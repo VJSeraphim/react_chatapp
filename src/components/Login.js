bd3be852b06f817b3c3eabe0ab1128d72f050eaa
VJS <minsung_96@naver.com>
2021-07-06 22:30:47 +0900

    chatapp #1

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons'
+import "firebase/app"
+
+import { auth } from '../firebase'
+import firebase from 'firebase/app'
 
 const Login = () => {
     return (
@@ -7,11 +11,17 @@ const Login = () => {
             <div id="login-card">
                 <h2>Welcome to the Chat!</h2>
 
-                <div className="login-button google">
+                <div 
+                    className="login-button google"
+                    onClick = {() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
+                >
                     <GoogleOutlined /> Sign In with Google
                 </div>
                 <br /><br />
-                <div className="login-button facebook">
+                <div 
+                    className="login-button facebook"
+                    onClick = {() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}
+                >
                     <FacebookOutlined /> Sign In with Facebook
                 </div>
             </div>
