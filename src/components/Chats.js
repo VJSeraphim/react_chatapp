6258f0643c47102383d693c86621caeb65fa4786
VJS <minsung_96@naver.com>
2021-07-08 23:39:35 +0900

    error fixed

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,21 +1,52 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import { ChatEngine } from 'react-chat-engine'
 import { auth } from '../firebase'
 
 import { useAuth } from '../contexts/AuthContext'
+import axios from 'axios'
 
 const Chats = () => {
     const history = useHistory()
     const { user } = useAuth()
-
-    console.log(user)
+    const [loading, setLoading] = useState(true)
 
     const handleLogout = async() =>{
         await auth.signOut()
 
         history.push('/')
     }
+
+    const getFile = async (url) => {
+        const response = await fetch(url)
+        const data = await response.blob()
+
+        return new File([data], "userPhoto.jpg", { type: 'image/jpeg' })
+    }
+
+    useEffect(() =>{
+        if(!user) {
+            history.push('/')
+            return
+        }
+
+        axios.get('https://api.chatengine.io/users/me', {
+            headers: {
+                "project-id": "a34c0ea8-cc20-48a9-afa6-e79dbf008c97",
+                "user-name" : user.email,
+                "user-secret": user.uid,
+            }
+        })
+        .then(() =>{
+            setLoading(false)
+        })
+        .catch(() =>{
+            let formdata = new Formdata()
+            formdata.append('email', user.email)
+            formdata.append('username', user.email)
+            formdata.append('secret', user.uid)
+        })
+    }, [user, history])
 
     return (
         <div className="chats-page">
